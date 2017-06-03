0b77c42f37e67d0a28ac5efaade4a09d15fab251
maowei17 <470419503@qq.com>
2017-06-03 12:02:06 +0800

    添加了两个按钮，并且能够响应相应的事件

--- ihealthEx/MainWnd.h
+++ ihealthEx/MainWnd.h
@@ -1,9 +1,17 @@
 #pragma once
-#include "UILib.h"
+#include "UIlib.h"
 
 class CMainWnd : public DuiLib::WindowImplBase {
+//UI≥ı ºªØ
 public:
 	virtual LPCTSTR GetWindowClassName() const;
-
 	virtual DuiLib::CDuiString GetSkinFile();
+	void InitWindow();
+
+public:
+	void Notify(DuiLib::TNotifyUI &msg);
+
+private:
+	DuiLib::CButtonUI *m_pStartBtn;
+	DuiLib::CButtonUI *m_pStopBtn;
 };
