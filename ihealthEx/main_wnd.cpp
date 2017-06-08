1735cff3cde55699f9ae7eb418bfb79cccf8a083
maowei17 <470419503@qq.com>
2017-06-08 10:01:28 +0800

    test interrupt

--- ihealthEx/main_wnd.cpp
+++ ihealthEx/main_wnd.cpp
@@ -20,16 +20,30 @@ void CMainWnd::InitWindow() {
 	freopen("CONOUT$", "w", stdout);
 }
 
+I32 interrupt_no;
 void CMainWnd::Notify(TNotifyUI &msg) {
 	CDuiString name = msg.pSender->GetName();
 	if (msg.sType == _T("click")) {
 		if (name.CompareNoCase(_T("btnInitial")) == 0) {
 			m_pControlCard = new ControlCard();
 			m_pControlCard->Initial();
+			m_pControlCard->PositionReset();
+
+	
+			I32 ret;
+			I32 factor = (1 << 12);
+			interrupt_no = APS_set_int_factor(0, 0, factor, 1);
+			APS_int_enable(0, 1);
 		} else if (name.CompareNoCase(_T("btnPositive")) == 0) {
-			m_pControlCard->MotorVelocityMove(SHOULDER_AXIS_ID, 2);
+			m_pControlCard->SetClutch(CLUTCH_ON);
+			m_pControlCard->SetMotor(MOTOR_ON);
+			APS_absolute_move(0, 2 / 0.009, 2 / 0.009);
+			APS_wait_single_int(interrupt_no, -1);
+			AllocConsole();
+			freopen("CONOUT$", "w", stdout);
+			printf("be in place");
 		} else if (name.CompareNoCase(_T("btnNegative")) == 0) {
-			APS_stop_move(SHOULDER_AXIS_ID);
+			
 		} else if (name.CompareNoCase(_T("btnStop")) == 0) {
 			m_pControlCard->PositionReset();
 		}
